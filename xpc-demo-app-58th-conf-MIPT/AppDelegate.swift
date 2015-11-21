d3f2b513755c560ab58834114e08f1b588e9a285
Ruslan Shaydulin <ruslan@l2sh.ru>
2015-11-21 22:38:06 +0300

    Initial Commit

---
+++ xpc-demo-app-58th-conf-MIPT/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  xpc-demo-app-58th-conf-MIPT
+//
+//  Created by Руслан on 21.11.15.
+//  Copyright © 2015 MIPT. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ xpc-demo-app-58th-conf-MIPT/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  xpc-demo-app-58th-conf-MIPT
+//
+//  Created by Руслан on 21.11.15.
+//  Copyright © 2015 MIPT. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
