495ad691359fbec5c1b6d1aa889c4a6f1322b4dc
Ruslan Shaydulin <ruslan@l2sh.ru>
2015-11-24 13:30:34 +0300

    other xpc file added (forget to check boxes in previous commit)

---
+++ ImageLoadHelper/ImageLoadHelper.m
@@ -0,0 +1,21 @@
+//
+//  ImageLoadHelper.m
+//  ImageLoadHelper
+//
+//  Created by Руслан on 24.11.15.
+//  Copyright © 2015 MIPT. All rights reserved.
+//
+
+#import "ImageLoadHelper.h"
+#import "ImageDownloader.h"
+
+@implementation ImageLoadHelper
+
+// This implements the example protocol. Replace the body of this class with the implementation of this service's protocol.
+- (void)loadImage:(NSURL *)url withReply:(void (^)(NSImage *))reply {
+    ImageDownloader* myLoader = [[ImageDownloader alloc] initWithURL:url];
+    myLoader.loadImage;
+    reply(myLoader.image);
+}
+
+@end
